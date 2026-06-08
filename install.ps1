deeba4cc918eed75d0f813892b55cdb194d2f5d5
supergiter <niysbila@gmail.com>
2026-06-08 21:28:31 +0800

    Point installer to pairing guard payload

--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-$BridgePayloadRef = "25d4a7c8db164361da1186a88b807a525ca7c137"
+$BridgePayloadRef = "69ea4370ba2e0aa6ef62610f84a080c9fc22f64b"
 $RepoRawBase = "https://raw.githubusercontent.com/2922062699-beep/hermes-mobile-bridge/$BridgePayloadRef"
 $InstallRoot = Join-Path $env:LOCALAPPDATA "HermesMobileBridge"
 $InstallRequestId = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
